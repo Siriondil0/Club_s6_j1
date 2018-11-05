1aec116ce665a56a61d028ab851e90053438e2f8
guillaume <elros.du.13@gmail.com>
2018-11-05 11:05:53 +0100

    club

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,14 +5,16 @@ class RegistrationsController < ApplicationController
   end
   
   def create 
-    if User.where(email: params[:user][:email]).first
-      redirect_to '/registrations', flash: {notice: "Email déjà utilisé"}
-    else
-      @user = User.create!(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password])
+    @user = User.create(first_name: params[:user][:first_name], last_name: params[:user][:last_name], email: params[:user][:email], password: params[:user][:password])
+    if @user.save
       session[:user_id] = @user.id
       redirect_to '/users'
+    else
+      flash[:notice] = @user.errors.full_messages
+      redirect_to '/registrations'
     end
   end
+
   def show
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 	end
 
 	def fight_club 
+		@user=User.all
 		unless session[:user_id]
 			redirect_to '/users', flash: {notice: "Accès interdit au club sans être connecté"}
 		end
