d51e441aaa0d316e80e38d0cc2cfd45cc3d46cc1
guillaume <elros.du.13@gmail.com>
2018-11-05 16:26:39 +0100

    add link content

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,15 +4,27 @@ RSpec.describe UsersController, type: :controller do
 	render_views
 
 	context "Get index" do
-		render_views false
-		it 'return a success response' do 
+		
+		it 'return a success response and have register and connect link ' do 
 			get :index
 			expect(response).to be_success
+			expect(response.body).to have_link('Enregistre-toi !')
+			expect(response.body).to have_link('Connecte-toi !')
+		end
+
+		it 'return a success response and have club and disconnect link' do
+			user = User.create!(first_name: 'toto', last_name: 'Toto', email: 'toto@mail.com', password: "01234")
+			login(user)
+			get :index
+			expect(response).to be_success
+			expect(response.body).to have_link('Le club secret :)')
+			expect(response.body).to have_link('Déconnexion')
 		end
 	end
 
 	context "Get club" do
 		render_views false
+
 		it 'return a redirect if not connected' do
 			get :fight_club
 			expect(flash[:notice]).to be_present
@@ -28,13 +40,14 @@ RSpec.describe UsersController, type: :controller do
 	end
 
 	context "Get show" do
+
 		it 'redirect if not connected' do
 			user = User.create!(first_name: 'toto', last_name: 'Toto', email: 'toto@mail.com', password: "01234")
 			get :show, params: {id: user.to_param}
 			expect(flash[:notice]).to be_present
 			expect(response).to redirect_to '/users'
 		end
-		
+
 		it 'redirect if not connected with right user' do
 			user = User.create!(first_name: 'toto', last_name: 'Toto', email: 'toto@mail.com', password: "01234")
 			user2 = User.create!(first_name: 'toto', last_name: 'Toto', email: 'toto2@mail.com', password: "01234")
@@ -53,8 +66,11 @@ RSpec.describe UsersController, type: :controller do
 			expect(response.body).to include user.last_name
 			expect(response.body).to include user.email
 		end
-		
 	end
+
+	context "Get edit" do
+
+
 end
 	
 
