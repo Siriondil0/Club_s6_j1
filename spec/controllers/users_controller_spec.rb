7c58616af376244020b5b78b3168099651448955
guillaume <elros.du.13@gmail.com>
2018-11-05 12:06:49 +0100

    test

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+	context "Get index" do
+		it 'return a success response' do 
+			get :index
+			expect(response).to be_success
+		end
+	end
+
+	context "Get club" do
+		it 'return a success response' do
+			get :fight_club
+			expect(response).to be_success
+		end
+	end
+	
+	
+
+end
