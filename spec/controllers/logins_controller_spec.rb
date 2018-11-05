9500219a23962db662c561b0a0ec4e56d84a137a
Hai-Dang <haidang.hathuc@gmail.com>
2018-11-05 21:05:57 +0100

    modif indentation

--- spec/controllers/logins_controller_spec.rb
+++ spec/controllers/logins_controller_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe LoginsController, type: :controller do
 
   context "GET #index" do
     it 'returns a success response' do 
-	  get :index
-	  expect(response).to be_successful
-	end
+	    get :index
+	    expect(response).to be_successful
+	  end
   end
 
   context "POST #create" do
@@ -18,7 +18,7 @@ RSpec.describe LoginsController, type: :controller do
     end
   end
 
-    it 'returns an error due to invalid passwod' do
+    it 'returns an error due to invalid password' do
       user = User.create!(first_name: 'toto', last_name: 'Toto', email: 'toto@mail.com', password: "01234")
       post :create , params: {"user"=> { "email"=>'toto@mail.com',"password"=> "abcdef" }}
       expect(response).to redirect_to '/logins'
