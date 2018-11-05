75031bdb21e8fcae86f2966e17b8117507162516
Hai-Dang <haidang.hathuc@gmail.com>
2018-11-05 15:18:27 +0100

    update test registration controller spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
+  
+  context "GET #index" do
+    it 'returns a success response' do 
+	  get :index
+	  expect(response).to be_success
+	end
+  end
+
+  context "POST #create" do
+    it 'creates user with existing email' do
+      user = User.create!(first_name: 'First', last_name: 'Last', email: 'first.last@example.com', password: 'AZERTY12345')
+      post :create, params: { "user"=> { "first_name" => "First", "last_name" => "Last", "email" => "first.last@example.com", "password" => "AZERTY12345"} }
+      expect(response).to redirect_to '/registrations'
+    end
+  end
+
+  context "POST #create" do
+  it 'creates user with empty field' do
+    post :create, params: { "user"=> { "first_name" => "    ", "last_name" => "Last", "email" => "first.last@example.com", "password" => "AZERTY12345"} }
+    expect(response).to redirect_to '/registrations'
+  end
+end
 
 end
